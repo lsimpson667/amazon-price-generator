b1f84b38d164871f7383fceb4465fbd76ff544c0
Lois Simpson <lsimpson667@gmail.com>
2020-03-20 20:05:20 -0400

    refactored price generator using a function

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,48 +1,49 @@
 
 let itemName = prompt("What is the name of the item?");
-
 const basePrice = prompt("What is the base price of the item?");
-
 let blackFriday = prompt("Is today Black Friday?");
-
 let searchEngine = prompt("Did you find this product through a seach engine?");
-
 let comparisonShopping = prompt("Did you visit a comparison-shopping site?");
 
-// create message with user entries
-let msg = `Item Name: ${itemName} \n Base Price: ${basePrice} \n Black Friday: ${blackFriday} \n Search Engine: ${searchEngine} \n Comparison shopping: ${comparisonShopping}`;
-console.log(msg);
+// declare all final price variables and assign them with the value of the baseprice
+let finalPrice2 = basePrice; 
+let finalPrice3 = basePrice;
+let finalPrice4 = basePrice;
 
-// include final price in the if statemet
-let finalPrice = basePrice;
+// create equasions for each discount using the matching final price variable
+finalPrice2 = basePrice - (basePrice * .25);
+finalPrice3 = basePrice * 1.01;
+finalPrice4 = basePrice * .9;
 
-// if statement that increases or decreases the item (Use logic operators)
+// create function that shows a message based on which discount(s) will be provided to the user based on their imput to the prompted questions
+function discounts() {
+// create message to display user responses
+msg = `Item Name: ${itemName} \nBase Price: ${basePrice} \nBlack Friday: ${blackFriday} \nSearch Engine: ${searchEngine} \nComparison shopping: ${comparisonShopping} \n\n`;
+// create message to display for each type of discount
+msg2 = `It is Black Friday, so we will reduce the price by 25%.The final price is ${finalPrice2}\n`;
+msg3 = `The purchaser came through a search engine, so we will increase the price by 1%. The final price is ${finalPrice3}\n`;
+msg4 = `The purchaser visited a comparison-shopping site, so we will reduce the price by 10%. The final price is ${finalPrice4}\n`; 
 
-if (blackFriday === "yes") {
-    finalPrice = basePrice - (basePrice * .25);
-    msg = `${msg} It is Black Friday, so we will reduce the price by 25%.The final price is ${finalPrice}\n`;
-    
+// create if statement using logic operators with every possible combination of user inputs
+// alert the messages that correspond with each possible combination of user inputs
+    if (blackFriday === "yes" && searchEngine === "yes" && comparisonShopping === "yes") {
+        alert(`${msg} ${msg2} ${msg3} ${msg4}`);
+    } else if (blackFriday === "yes" && searchEngine === "yes") {
+        alert(`${msg} ${msg2} ${msg3}`);
+    } else if (blackFriday === "yes" && comparisonShopping === "yes") {
+        alert(`${msg} ${msg2} ${msg4}`);    
+    } else if (blackFriday === "yes") {
+        alert(`${msg} ${msg2}`);
+    } else if (searchEngine === "yes" && comparisonShopping === "yes") {
+        alert(`${msg} ${msg3} ${msg4}`);
+    } else if (searchEngine === "yes") {
+        alert(`${msg} ${msg3}`)
+    } else if (comparisonShopping === "yes") {
+        alert(`${msg} ${msg4}`);
+    }
 }
 
-if (searchEngine === "yes") {
-    finalPrice = basePrice * 1.01;
-    msg = `${msg} The purchaser came through a search engine, so we will increase the price by 1%. The final price is ${finalPrice}\n`;
-    
-}
-
-if (comparisonShopping === "yes") {
-    finalPrice = basePrice * .9;
-    msg = `${msg} The purchaser visited a comparison-shopping site, so we will reduce the price by 10%. The final price is ${finalPrice}\n`; 
-    
-};
-
-
-// Alert a message that coresponds with yes or no answers
-alert(msg)
-
-
-
-
+discounts();
 
 
 
